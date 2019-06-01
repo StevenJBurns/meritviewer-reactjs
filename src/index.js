18feed8c766911b76e782dc28352cc592fa2f1b9
StevenJBurns <stevenjburns@msn.com>
2019-06-01 07:26:10 -0600

    chore: clean up boilerplate in index.js and index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+/* import * as serviceWorker from './serviceWorker'; */
+import App from './app/App';
 import './index.css';
 
 ReactDOM.render(
@@ -8,5 +9,4 @@ ReactDOM.render(
   document.getElementById('react-root')
 );
 
-import * as serviceWorker from './serviceWorker';
-serviceWorker.unregister();
+/* serviceWorker.unregister(); */
