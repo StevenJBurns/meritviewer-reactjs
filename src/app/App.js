fb0143f2911ab7789a2aa8c61cbd199d5712afa3
StevenJBurns <stevenjburns@msn.com>
2019-06-01 17:40:05 -0600

    refacor: use props.children composition in App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import AppLogo from '../assets/react-logo.svg';
+// import AppLogo from '../assets/react-logo.svg';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={AppLogo} className="App-logo" alt="logo" />
-      <p>Edit <code>src/App.js</code> and save to reload.</p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-    </header>
-  </div>
+const App = props => (
+  // <div className="App">
+  //   <header className="App-header">
+  //     <h1>PlanetSide MeritViewer</h1>
+  //     <img src={AppLogo} className="App-logo" alt="logo" />
+  //     <p>Edit <code>src/App.js</code> and save to reload.</p>
+  //     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+  //   </header>
+  // </div>
+  <React.Fragment>
+    { props.children }
+  </React.Fragment>
 );
 
 export default App;
