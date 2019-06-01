d78a993ca153ea16d0f58e41c42875303dc0208f
StevenJBurns <stevenjburns@msn.com>
2019-06-01 17:40:38 -0600

    feat: use AppHeader and AppFooter in index.js; use Coalition font

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 /* import * as serviceWorker from './serviceWorker'; */
 import App from './app/App';
+import { AppHeader } from './app/AppHeader/AppHeader';
+import { AppFooter } from './app/AppFooter/AppFooter'
+
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <App>
+    <AppHeader />
+    <main />
+    <AppFooter />
+  </App>,
   document.getElementById('react-root')
 );
 
