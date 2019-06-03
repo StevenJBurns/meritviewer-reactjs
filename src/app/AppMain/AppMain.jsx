83e2789e9780b6649ab5a4280e6810291df8d436
StevenJBurns <stevenjburns@msn.com>
2019-06-02 19:30:04 -0600

    feat: create component, style and test files for AppMain

---
+++ src/app/AppMain/AppMain.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const AppMain = props => {
+  return (
+    <main></main>
+  );
+};
---
+++ src/app/AppMain/AppMain.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import { AppMain } from '.';
+
+describe('<AppMain />', () => {
+  // afterEach(cleanup);
+
+  it('renders without errors', () => {
+    // const {container, debug } = render(<AppMain />);
+    // debug();
+  });
+});
