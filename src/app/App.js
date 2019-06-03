55913542c0a620a9bf2e7a8c141434526f332b60
StevenJBurns <stevenjburns@msn.com>
2019-06-02 19:31:22 -0600

    refactor: App.js uses all app JSON data; context provider added but not functional yet

--- src/app/App.js
+++ src/app/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-// import AppLogo from '../assets/react-logo.svg';
+import AppData from '../context/meritviewer.json';
+import { AppDataContext } from '../context/AppDataContext';
 import './App.css';
 
-const App = props => (
-  // <div className="App">
-  //   <header className="App-header">
-  //     <h1>PlanetSide MeritViewer</h1>
-  //     <img src={AppLogo} className="App-logo" alt="logo" />
-  //     <p>Edit <code>src/App.js</code> and save to reload.</p>
-  //     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-  //   </header>
-  // </div>
-  <React.Fragment>
-    { props.children }
-  </React.Fragment>
-);
+const App = props => {
+  const [appData, setAppData] = React.useState(AppData);
+
+  React.useEffect(() => {
+    console.log(appData);
+  }, []);
+  
+  return (
+    <AppDataContext.Provider value={appData}>
+      { props.children }
+    </AppDataContext.Provider>
+  );
+};
 
 export default App;
