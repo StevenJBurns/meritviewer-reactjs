b38cf22db23a8d6a430269d2706f4f1a81b37e08
StevenJBurns <stevenjburns@msn.com>
2019-06-02 23:28:54 -0600

    feat: create component, spec and style files for Group with minor boilerplate

---
+++ src/components/Group/Group.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Group.scss';
+
+export const Group = props => {
+  const { title } = props;
+
+  return (
+    <React.Fragment>
+        <span>{ title }</span>
+        { props.children}
+    </React.Fragment>
+  );
+};
