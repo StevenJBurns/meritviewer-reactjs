3fba4dbc11d25eb8eed40784f8605ad43cb383be
StevenJBurns <stevenjburns@msn.com>
2019-06-02 23:29:04 -0600

    feat: create component, spec and style files for Merit with minor boilerplate

---
+++ src/components/Merit/Merit.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Merit.scss';
+
+export const Merit = props => {
+  const { title } = props;
+
+  return (
+    <span>{ title }</span>
+  );
+};
