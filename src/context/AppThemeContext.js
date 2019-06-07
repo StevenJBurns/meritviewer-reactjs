6a86898284d6b92f9677e9f527a2ea3db33b385d
StevenJBurns <stevenjburns@msn.com>
2019-06-06 23:31:28 -0600

    feat: create AppThemeContext boilerplate

---
+++ src/context/AppThemeContext.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const changeEmpireTheme = nextEmpire => {
+
+};
+
+const themeReducer = (state, action) => {
+  
+}
+
+export const AppThemeContext = React.createContext();
