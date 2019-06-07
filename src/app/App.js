d2a4be7e14c71c70de97587e88c3a57ed1a2ea8f
StevenJBurns <stevenjburns@msn.com>
2019-06-06 23:34:25 -0600

    feat: create AppMain component with context for theme

--- src/app/App.js
+++ src/app/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import AppData from '../context/meritviewer.json';
 import { AppDataContext } from '../context/AppDataContext';
+import { AppThemeContext } from '../context/AppThemeContext';
 import './App.css';
 
 const App = props => {
+  const [theme, setTheme] = React.useState();
   const [appData, setAppData] = React.useState(AppData);
 
   React.useEffect(() => {
-    console.log(appData);
-  }, []);
+    console.log(theme);
+  }, [theme]);
   
   return (
-    <AppDataContext.Provider value={appData}>
+    <AppThemeContext.Provider value={appData}>
       { props.children }
-    </AppDataContext.Provider>
+    </AppThemeContext.Provider>
   );
 };
 
