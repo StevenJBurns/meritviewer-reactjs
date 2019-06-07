e27dc674ad497386f96fd45bd222b157f3b8a3c4
StevenJBurns <stevenjburns@msn.com>
2019-06-06 23:33:26 -0600

    feat: use ThemeFilterButton components in AppMain

--- src/app/AppMain/AppMain.jsx
+++ src/app/AppMain/AppMain.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ThemeFilterButton } from '../../components/ThemeFilterButton/ThemeFilterButton';
 
 export const AppMain = props => {
   return (
-    <main></main>
+    <main>
+      <ThemeFilterButton empire="ALL" />
+      <ThemeFilterButton empire="TR" />
+      <ThemeFilterButton empire="NC" />
+      <ThemeFilterButton empire="VS" />
+      { props.children }
+    </main>
   );
 };
