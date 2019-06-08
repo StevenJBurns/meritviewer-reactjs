43fe67301b153a64b9cf27bfa4f68560f3744087
StevenJBurns <stevenjburns@msn.com>
2019-06-08 17:10:16 -0600

    feat: create EmpireFilterToolbar component and SCSS to hold filter buttons

---
+++ src/components/EmpireFilterToolbar/EmpireFilterToolbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './EmpireFilterToolbar.scss';
+
+export const EmpireFilterToolbar = props => {
+  return (
+    <div>
+      { props.children }
+    </div>
+  );
+};
+
