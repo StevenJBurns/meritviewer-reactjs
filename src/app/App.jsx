e3a21f8afec4dbccdd326f28bb86519de3c9d54e
StevenJBurns <stevenjburns@msn.com>
2019-06-15 21:05:09 -0600

    refactor: change app JSON data to an array; rewite map() and filter() logic on empire changes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import AppData from '../data/meritviewer.json';
-// import { AppDataContext } from '../context/data/AppDataContext';
 import { EmpireThemeContext } from '../context/theme/empireThemeContext';
 import { themes } from '../context/theme/empireThemes';
 import './App.scss';
 
 export const App = props => {
-  const [theme, setTheme] = React.useState('');
+  const [appData, setAppData] = React.useState(AppData);
+  const [empire, setEmpire] = React.useState('ALL')
+  const [theme, setTheme] = React.useState({});
 
-  const changeTheme = newTheme => {
-    setTheme(themes[newTheme]);
-    localStorage.setItem('empireTheme', newTheme);
+  const changeTheme = newEmpire => {
+    setEmpire(newEmpire);
+    localStorage.setItem('empire', newEmpire);
+
+    if(newEmpire === 'ALL') return setAppData(AppData);
+
+    let filteredMerits = AppData.map(category => 
+      ({...category, groups: category.groups.map(group =>
+        ({...group, merits: group.merits.filter(merit =>
+          merit.empire === empire || merit.empire === 'ALL'
+        )})
+      )})
+    )
+    setAppData(filteredMerits);
   };
 
   React.useEffect(() => {
-    const localStorageEmpire = localStorage.getItem('empireTheme') || 'ALL';
+    const localStorageEmpire = localStorage.getItem('empire') || 'ALL';
     changeTheme(localStorageEmpire);
-  }, [])
+  }, []);
+
+  React.useEffect(() => {
+    console.log('empire: ', empire);
+    console.log('merits: ', appData[0].groups[0].merits);
+    setTheme(themes[empire]);
+
+  }, [empire]);
 
   return (
     <EmpireThemeContext.Provider value={{ theme, changeTheme }}>
