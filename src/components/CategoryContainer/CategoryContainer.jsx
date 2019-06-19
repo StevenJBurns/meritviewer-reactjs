67cb5243aa39e6e18fcf4a890be7afe3cab1a62d
StevenJBurns <stevenjburns@msn.com>
2019-06-19 17:25:52 -0600

    feat: basic data pull of merit categories with useContext from AppData into CategoryContainer

--- src/components/CategoryContainer/CategoryContainer.jsx
+++ src/components/CategoryContainer/CategoryContainer.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { AppDataContext } from '../../context/data/AppDataContext';
+import './CategoryContainer.scss';
 
 export const CategoryContainer = props => {
-  const [] = React.useState();
+  const { currentAppData } = React.useContext(AppDataContext);
+
   return (
-    <section>
-      { props.children }
-    </section>
+      <ul id="category-container">
+        { currentAppData.map((category, index) => 
+          <li key={index} className="category-container__item">{category.title}</li>
+          )
+        }
+        { props.children }
+      </ul>
   );
 };
 
 CategoryContainer.propTypes = {
   children: PropTypes.oneOf([
     PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
+    PropTypes.node,
   ]),
 };
 
