a61c293a7f9cd0eb2a8fec7bd858dd38a9bd6e1e
StevenJBurns <stevenjburns@msn.com>
2019-06-19 17:26:38 -0600

    feat: create basic boilerplate for GroupContainer, SCSS and spec file

---
+++ src/components/GroupContainer/GroupContainer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './GroupContainer.scss';
+
+export const GroupContainer = props => {
+  return (
+    <section id="group-container">
+
+    </section>
+  );
+};
+
+GroupContainer.propTypes = {
+
+};
+
+GroupContainer.defaultProps = {
+
+};
