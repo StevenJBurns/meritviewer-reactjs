a1aeb2593a26692e4a2f1bcd165223fe279454d2
StevenJBurns <stevenjburns@msn.com>
2019-07-04 23:56:08 -0600

    chore: refactor empire styles in JSON slightly

--- src/context/theme/empireThemes.js
+++ src/context/theme/empireThemes.js
@@ -1,25 +1,25 @@
 export const themes = {
   ALL: {
 		colorDark: '#F0F0F0',
-		colorLight: '',
+		colorLight: '#F0F0F0',
 		backgroundDark: '#303030',
 		backgroundLight: '#D0D0D0',
 	},
 	TR: {
-		color: '#F0F0F0',
-		colorLight: '',
+		colorDark: '#F0F0F0',
+		colorLight: '#F0F0F0',
 		backgroundDark: 'darkred',
 		backgroundLight: 'red',
 	},
 	NC: {
-		color: 'gold',
-		colorLight: '',
+		colorDark: 'gold',
+		colorLight: '#F0F0F0',
 		backgroundDark: '#003366',
-		backgroundLight: '#c6e3ff',
+		backgroundLight: '#C6E3FF',
 	},
 	VS: {
-		color: '#B6FFAF',
-		colorLight: '',
+		colorDark: '#B6FFAF',
+		colorLight: '#F0F0F0',
 		backgroundDark: '#300070',
 		backgroundLight: '#FF93FF',
 	},
